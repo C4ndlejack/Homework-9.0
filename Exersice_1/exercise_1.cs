e7829a3610fe61e6945325b20233392dda08ab6a
Candlejack <garouspeak@gmail.com>
2022-09-06 19:17:18 +0300

    exercise 1 is done

---
+++ Exersice_1/exercise_1.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+using System;
+{
+    Console.Write("Введите число N: ");
+    int N = Convert.ToInt16(Console.ReadLine());
+    Console.Write("Введите число M: ");
+    int M = Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine($"Все натуральные числа в промежутке: ");
+    if (M > N)
+    for (int i = N; i <= M; i++)
+        Console.Write($" {i}");
+    else
+        for (int i = M; i <= N; i++)
+            Console.Write($" {i}");
+}
